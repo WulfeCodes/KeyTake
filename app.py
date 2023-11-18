3e5ac695b73b6b1ee2ae0e9fc12938dadc7d0173
Raphael Palacio <54969177+raphaelpalacio@users.noreply.github.com>
2023-11-18 17:52:17 -0500

    FIXED FLASK APP

--- app.py
+++ app.py
@@ -1,88 +1,67 @@
-
-
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask import Flask, jsonify, render_template
 import requests
-from pymongo import MongoClient
+import json
 
 app = Flask(__name__)
-CORS(app)
 
-# MongoDB setup
-client = MongoClient('mongodb://localhost:27017/')
-db = client.groupme
-action_items_collection = db.action_items
+# constants
+GM_API_KEY = 'mlYGolL23Ch5uEEhTchiumiNGR15JqSWWiGyCwni'
 
-# OneAI API call
-def process_oneai(text):
-    url = "https://api.oneai.com/api/v0/pipeline"
+# fetching the group_ids
+def fetchGroupID(access_token):
+    url = 'https://api.groupme.com/v3/groups'
     headers = {
-        'api-key': '', # OneAI API KEY Here
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'X-Access-Token': access_token,
     }
-    payload = {
-        "input": text,
-        "steps": [
-            {"skill": "action-items"},
-        ]
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    return response.json()  # Make sure to handle errors and status codes in production
 
-def fetchGroupID():
-    url = f"https://api.groupme.com/v3/groups"
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
-            data = response.json()
-            groups = data['response']
-            for group in groups:
-                print(f"Group Name: {group['id']}, Group ID: {group['name']}")
+        return response.json()
     else:
-        print(f"Error {response.status_code}: {response.text}")
+        print(f"Failed to get groups. Status code: {response.status_code}")
+        return None
 
-# GroupMe API call
-def fetch_groupme_messages(group_id):
-    url = f"https://api.groupme.com/v3/groups/{group_id}/messages"
-    params = {
-        'token': 'YOUR_GROUPME_ACCESS_TOKEN',
-        'limit': 100  
-    }
-    response = requests.get(url, params=params)
-    messages = response.json().get('response', {}).get('messages', [])
-    # Extract the text from the messages
-    text = ' '.join([msg['text'] for msg in messages if msg['text'] is not None])
-    return text
+@app.route("/fetch_group_id")
+def fetch_group_id():
+    group_id = fetchGroupID(access_token=GM_API_KEY)
+    return group_id
+
+# -------------------------------------------------------------------
+
+# fetching the group_messages
+def getMessages(group_id):
+
+    data = fetchGroupID(access_token=GM_API_KEY) # a dictionary
+
+    for group in data:
+        if group["id"] == group_id:
+            return group['message']
+
+@app.route("/get_messages")
+def get_messsages():
+    group_id = getMessages(633819540)
+    return group_id
+
+# -------------------------------------------------------------------
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 
-
-@app.route('/process_groupme_messages', methods=['POST'])
-def process_groupme_messages():
-    data = request.json
-    group_id = data['group_id']
-    print(group_id)
-
-    # Fetch messages from GroupMe
-    messages = fetch_groupme_messages(group_id)
-
-    # Process messages through OneAI
-    processed_data = process_oneai(messages)
-
-    # Store in MongoDB
-    action_items_collection.insert_many(processed_data)
-
-    return jsonify({"status": "success", "data": processed_data})
-
-# Additional routes can be added as needed
-
-def main():
-     fetchGroupID()
-
-if __name__ == '__main__':
-    # app.run(debug=True)
-    # fetchGroupID()
-    main()
-
-@app.route("/")    
+@app.route("/home")
 def home():
-     return "<p> Test file </p>"
+    group_id = fetch_group_id() # it's  a dictionary.
+    return render_template('home.html', group_id=group_id)
 
+
+@app.route("/login")
+def login():
+    return "<h1>login page</h1>"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
